61232202e749853ccb0d67a3c36c066bf3dd8fa4
Anas Hassar (300228772) <anas.hassar86@gmail.com>
2026-06-22 11:42:36 -0700

    Initial Commit

---
+++ src/test/java/com/cmpt276/SFSS/Nexus/SfssNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpt276.SFSS.Nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfssNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
